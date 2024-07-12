c5898c2d99d083bfffc7d970b25d8cbb805bb347
cwp0 <wenpengchen@njust.edu.cn>
2024-07-12 20:05:51 +0800

    修改套餐代码导入

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -70,4 +70,13 @@ public interface SetmealMapper {
     @Delete("delete from setmeal where id = #{id}")
     void deleteById(Long setmealId);
 
+    /**
+     * 根据id修改套餐
+     *
+     * @param setmeal
+     */
+    @AutoFill(OperationType.UPDATE)
+    void update(Setmeal setmeal);
+
+
 }
